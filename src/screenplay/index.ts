e112888fd061acc18c5ee9aaf694b518914c7606
Andres <andresvz50@gmail.com>
2025-05-08 19:29:11 -0500

    feat: project setup and initial configuration 🎉

---
+++ src/screenplay/index.ts
@@ -0,0 +1,4 @@
+// export * from './actors/Actor';
+// export * from './interfaces/tasks/Task';
+// export * from './interfaces/questions/Question';
+// export * from './interfaces/Interaction'; // si lo necesitas luego
