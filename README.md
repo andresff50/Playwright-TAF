88390de5fcac5f9ad0792b43acb9afa5e1b7a218
Andres <andresvz50@gmail.com>
2025-12-01 22:36:48 -0500

    Update Playwright version 1.52.0 -> 1.57.0

---
+++ README.md
@@ -0,0 +1,1 @@
+Basic TAF for Playwright
--- src/screenplay/index.ts
+++
@@ -1,4 +0,0 @@
-// export * from './actors/Actor';
-// export * from './interfaces/tasks/Task';
-// export * from './interfaces/questions/Question';
-// export * from './interfaces/Interaction'; // si lo necesitas luego
